33d7b565fba10016905c1f551c9f081c2005bdf2
Andrew Liu <andrewliustudio@gmail.com>
2020-10-31 00:08:34 -0400

    feat: [@drewfle/bundler@v1.1] Added TS React

---
+++ drewfle-bundler-v1.1/ts-react/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./styles.css";
+
+ReactDOM.render(
+  <h1 className={styles.foo}>Hello, world!</h1>,
+  document.getElementById("root")
+);
